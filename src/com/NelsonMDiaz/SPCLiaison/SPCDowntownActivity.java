a4270b663eec54afee5f605f7bf3af49e730ed73
Nelson MD <nelsonmd81@yahoo.com>
2014-01-27 22:24:44 -0500

    Added 'Get Directions' button

--- src/com/NelsonMDiaz/SPCLiaison/SPCDowntownActivity.java
+++ src/com/NelsonMDiaz/SPCLiaison/SPCDowntownActivity.java
@@ -5,13 +5,48 @@ package com.NelsonMDiaz.SPCLiaison;
  */
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class SPCDowntownActivity extends FragmentActivity
 {
+    GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.spc_downtown_campus);
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded()
+    {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null)
+        {
+            // try to obtain the map from the supportmapfragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null)
+            {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap()
+    {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker").icon(BitmapDescriptorFactory.fromResource(R.drawable.pin)));
+
     }
 }
